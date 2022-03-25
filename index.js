631c99721ae01b4248d8f722b0e02b5e92420614
Jonny Howle <jonny.howle@gmail.com>
2022-03-25 13:05:57 -0600

    commit tree 0ed52b3eb00e6a6fe4ae9e2f1a3e1fa1424fdf47

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const drivers = [ "Alice", "Bob", "Jane", "John" ];
+
+let fare = 40;
+
+const returnFirstTwoDrivers = function(driversArray) {
+    return driversArray.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(driversArray) {
+    return driversArray.slice(-2);
+}
+
+let selectingDrivers = [
+    returnFirstTwoDrivers, 
+    returnLastTwoDrivers
+];
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return (fare * int);
+    }
+}
+
+const fareDoubler = function(fare) {
+    return (fare) * 2;
+}
+
+const fareTripler = function(fare) {
+    return (fare) * 3;
+}
+
+function selectDifferentDrivers(driversArray, functionReturn) {
+    return functionReturn(driversArray);
+}
+
+returnFirstTwoDrivers(drivers);
+
+returnLastTwoDrivers(drivers);
